dd76693e8e2dd460d57b47b6f8ce1872e548347f
Marco Magliona <marco.magliona@gmail.com>
2014-11-30 01:56:05 +0100

    smdk4412-common: fix RIL_init
    Enable RIL_init fix for samsung proprietary RIL lib (libsec-ril.so).
    This depends on https://gerrit.omnirom.org/#/c/10652/
    
    Change-Id: I70e31030c4fd06e45808bf29385cb30f3e77e7b5
    
    Conflicts:
    	BoardCommonConfig.mk

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -105,6 +105,8 @@ BOARD_USES_MFC_FPS := true
 BOARD_USE_S3D_SUPPORT := true
 BOARD_USE_CSC_FIMC := false
 
+BOARD_NEEDS_SEC_RIL_WORKAROUND := true
+
 # Wifi
 BOARD_WLAN_DEVICE                := bcmdhd
 BOARD_WLAN_DEVICE_REV            := bcm4334
